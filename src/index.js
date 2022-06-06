6efcc8d7529363e5f7ff68c8b0005dab97e64049
Suraj <suraj.bande98@gmail.com>
2022-06-06 17:49:04 +0530

    PWA integration

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
    
@@ -13,7 +15,10 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
